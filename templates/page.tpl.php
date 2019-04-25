8233405a686f687a1a074e1a6b11bb0def436fa7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-25 14:11:31 +0100

    A lot of new style and php changes all for the site

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -184,13 +184,12 @@
 <div class="main-container <?php print $container_class; ?>">
   <div class="container-fluid">
 	<div class="row">
+		<?php if (!empty($title)): ?>
+			<h1 class="page-header" id="page-main-heading"><?php print strstr($title, "®") ? str_replace("®", "<sup>®</sup>", $title) : $title; ?></h1>
+		<?php endif; ?>
 	 <div class="<?php if (empty($page['sidebar_first']) && empty($page['sidebar_second'])) { print 'col-sm-12 fullscreen'; }
 					else if (empty($page['sidebar_first']) || empty($page['sidebar_second'])) { print 'col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2'; }
 					else { print 'col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2'; } ?>">
-
-          <?php if (!empty($title)): ?>
-  					<h1 class="page-header" id="page-main-heading"><?php print strstr($title, "®") ? str_replace("®", "<sup>®</sup>", $title) : $title; ?></h1>
-					<?php endif; ?>
 
 			<div class="<?php if (empty($page['sidebar_first']) && empty($page['sidebar_second'])) { print 'col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2'; }
 														else if (empty($page['sidebar_first']) || empty($page['sidebar_second'])) { print 'col-sm-12'; }
@@ -316,7 +315,7 @@
 
 		<footer>
 			<?php if (!empty($page['footer'])): ?>
-			  <div class="footer <?php print $container_class; ?> footer-background">
+			  <div class="footer <?php print $container_class; ?>">
 				<?php print render($page['footer']); ?>
 			</div>
 			<?php endif; ?>
