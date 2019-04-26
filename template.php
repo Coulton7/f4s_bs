599a10f0b5b5a3cf0e47e768361a1bad16bb2bcc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-26 11:56:01 +0100

    Removing a script from the template.php may cause a delay on front page

--- template.php
+++ template.php
@@ -10,7 +10,6 @@ function f4s_bs_preprocess_page(&$vars) {
         $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
     }
 
-	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/logoscroll.js');
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/fade-text.js');
   drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/contact-icon.js');
   drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/reg-mark.min.js');
