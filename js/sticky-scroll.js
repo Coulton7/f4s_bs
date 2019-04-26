0ccf74a2a62c5e6023c7867214df24dee051eb4a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-26 11:25:34 +0100

    mass tidy up of script and css

--- js/sticky-scroll.js
+++ js/sticky-scroll.js
@@ -1,29 +1,35 @@
-(function($){
+(function($) {
 
-$(document).ready(function () {
+  $(document).ready(function() {
     "use strict";
     var nav = $('.navbar'),
-        body = $('body'),
-        windowScreen = $(window),
-        navOffset = nav.offset().top,
-        previousScroll = 0;
+      body = $('body'),
+      windowScreen = $(window),
+      navOffset = nav.offset().top,
+      previousScroll = 0;
 
     if (windowScreen.scrollTop() >= navOffset && !nav.hasClass('sticky')) {
-        nav.addClass('sticky');
-        $(body).css({paddingTop : nav.css('height')});
+      nav.addClass('sticky');
+      $(body).css({
+        paddingTop: nav.css('height')
+      });
     }
-    $(window).on('scroll', function () {
+    $(window).on('scroll', function() {
 
-        if (windowScreen.scrollTop() >= navOffset && !nav.hasClass('sticky')) {
-            nav.addClass('sticky');
-            $(body).css({paddingTop : nav.css('height')});
-        }
+      if (windowScreen.scrollTop() >= navOffset && !nav.hasClass('sticky')) {
+        nav.addClass('sticky');
+        $(body).css({
+          paddingTop: nav.css('height')
+        });
+      }
 
-        if (windowScreen.scrollTop() < navOffset && nav.hasClass('sticky')) {
-            nav.removeClass('sticky');
-            $(body).css({paddingTop : 0});
-        }
+      if (windowScreen.scrollTop() < navOffset && nav.hasClass('sticky')) {
+        nav.removeClass('sticky');
+        $(body).css({
+          paddingTop: 0
+        });
+      }
 
     });
-});
+  });
 })(jQuery);
