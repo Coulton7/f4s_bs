b01373da508c96a7aa8b6be9e2f15bdc141b89f3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-08-10 12:07:16 +0100

    Adding the js for googletagmanger

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -69,6 +69,15 @@
   <![endif]-->
   <?php print $scripts; ?>
 
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5HPR4KL');</script>
+<!-- End Google Tag Manager -->
+</script>
+
 </head>
 <body<?php print $body_attributes; ?>>
   <div id="skip-link">
